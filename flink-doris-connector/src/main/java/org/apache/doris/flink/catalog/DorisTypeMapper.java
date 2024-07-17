2dfd7e97b6546ea83b4776266fce9c351129a3ca
wudi <676366545@qq.com>
2024-07-17 15:52:12 +0800

    [Fix] unsupport when use doris catalog list views  (#432)
    (cherry picked from commit f92bc7541448f1a946567f00bf06d915a2e65ad7)

--- flink-doris-connector/src/main/java/org/apache/doris/flink/catalog/DorisTypeMapper.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/catalog/DorisTypeMapper.java
@@ -47,6 +47,7 @@ import org.apache.doris.flink.catalog.doris.DorisType;
 
 import static org.apache.doris.flink.catalog.doris.DorisType.ARRAY;
 import static org.apache.doris.flink.catalog.doris.DorisType.BIGINT;
+import static org.apache.doris.flink.catalog.doris.DorisType.BIGINT_UNSIGNED;
 import static org.apache.doris.flink.catalog.doris.DorisType.BOOLEAN;
 import static org.apache.doris.flink.catalog.doris.DorisType.CHAR;
 import static org.apache.doris.flink.catalog.doris.DorisType.DATE;
@@ -117,6 +118,7 @@ public class DorisTypeMapper {
                 // Here, return Array<string> as the type
                 return DataTypes.ARRAY(DataTypes.STRING());
             case LARGEINT:
+            case BIGINT_UNSIGNED:
             case STRING:
             case JSONB:
             case JSON:
